353ca65bd754abec85a9375e9b272de9792db9a0
Erik Cisneros <eca.experimental@gmail.com>
2024-12-20 20:21:06 -0800

    fix: Revise day 11 solution

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,71 +1,58 @@
 
 fun main() {
-    //val input = readInput("test11")
     val input = readInput("Day11")
-    //Day11.part1(input).println()
+    Day11.part1(input).println()
     Day11.part2(input).println()
 }
 
 object Day11 {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val stones  = input[0].split(" ")
-        return stones.blinkFor(25).size
+        var count = stones.getCounts()
+        repeat(25) {
+            count = count.processStep()
+        }
+        return count.values.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        val stones  = input[0].split(" ").toMutableList()
+    fun part2(input: List<String>): Long {
+        val stones  = input[0].split(" ")
+        var count = stones.getCounts()
         repeat(75) {
-            stones.processStones(stones.size)
+            count = count.processStep()
         }
-        return stones.size
+        return count.values.sum()
     }
-
-    fun List<String>.blinkFor(times: Int): List<String> {
-        var newStones = this
-        for(i in 0..<times) {
-            newStones = newStones.blink()
-        }
-        return newStones
-    }
-
-    fun List<String>.blink(): List<String> {
-        return this.map { stone ->
-            if(stone == "0")
-                stone.applyRule1()
-            else if(stone.length.isEven())
-                stone.appleRule2()
-            else
-                stone.applyRule3()
-        }.flatten()
-    }
-
-    fun String.applyRule1(): List<String> = listOf<String>("1")
-
-    fun String.appleRule2(): List<String> {
-        val left = (this.substring(0, this.length/2).toLong()).toString()
-        val right = (this.substring(this.length/2, this.length).toLong()).toString()
-        return listOf<String>(left, right)
-    }
-
-    fun String.applyRule3(): List<String> = listOf<String>((this.toLong() * 2024).toString())
 
     fun Int.isEven(): Boolean = (this.and(1) == 0)
 
-    fun MutableList<String>.processStones(count: Int): MutableList<String> {
-        for(i in 0..<count) {
-            if(this[i] == "0") {
-                this[i] = "1"
+    fun List<String>.getCounts(): Map<String,Long> {
+        val map = HashMap<String, Long>()
+        this.forEach { stone ->
+            map[stone] = map.getOrDefault(stone, 0) + 1
+        }
+        return map
+    }
+
+    fun Map<String, Long>.processStep(): Map<String, Long> {
+        val map = HashMap<String, Long>()
+        this.forEach { stone, count ->
+            if(stone == "0") {
+                val result = "1"
+                map[result] = map.getOrDefault(result, 0) + count
             }
-            else if(this[i].length.isEven()) {
-                val current = this[i]
-                this[i] = (current.substring(0, current.length/2).toLong()).toString()
-                this.add((current.substring(current.length/2, current.length).toLong()).toString())
+            else if(stone.length.isEven()) {
+                val result1 = (stone.substring(0, stone.length/2).toLong()).toString()
+                val result2 = (stone.substring(stone.length/2, stone.length).toLong()).toString()
+                map[result1] = map.getOrDefault(result1, 0) + count
+                map[result2] = map.getOrDefault(result2, 0) + count
             }
             else {
-                this[i] = (this[i].toLong() * 2024).toString()
+                val result = (stone.toLong() * 2024).toString()
+                map[result] = map.getOrDefault(result, 0) + count
             }
         }
-        return this
+        return map
     }
 }
 
